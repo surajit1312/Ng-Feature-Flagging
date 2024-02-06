8599064aaeb8d6e6d53b2f927a665adcd23bfe6a
Surajit Rana <surajit.rana@epicor.com>
2024-02-07 02:50:33 +0530

    feat[lazy-loading]: added lazy loaded route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ConfigurationComponent } from './configuration/configuration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, },
   { path: 'about', component: AboutComponent, },
-  { path: 'configuration', component: ConfigurationComponent, },
+  {
+    path: 'configuration', loadChildren: () =>
+      import('./configuration/configuration.module').then(m => m.ConfigurationModule)
+  },
 ];
 
 @NgModule({
