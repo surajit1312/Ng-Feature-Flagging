dc09b8d9490785205e035be139414c3dac344955
Surajit Rana <surajit.rana@epicor.com>
2024-02-07 02:53:42 +0530

    feat[lazy-loading]: added lazy loaded route

---
+++ src/app/configuration/configuration.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfigurationComponent } from './configuration.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ConfigurationComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    ConfigurationComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class ConfigurationModule { }
